d0098e86670cb14c85d1d115e51da57879e551cf
Chirantan Mukherjee <chirantanmukherjee29@gmail.com>
2023-02-09 00:19:08 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,7 +15,7 @@ $('#lan').hide();
 
 $(document).ready(function(){
 
-	$.getJSON("https://api.countapi.xyz/hit/publish.uwo.ca/~cmukher/", function(response) {
+	$.getJSON("https://api.countapi.xyz/hit/https://publish.uwo.ca/~cmukher/", function(response) {
 		$("#contadorVisitas").text(response.value);
 	});
 
@@ -361,7 +361,7 @@ function scrollToContent(divId) {
 }
 
 function resetViews() {
-	$.getJSON("https://api.countapi.xyz/set/publish.uwo.ca/~cmukher/", function(response) {
+	$.getJSON("https://api.countapi.xyz/set/https://publish.uwo.ca/~cmukher/", function(response) {
 		$("#contadorVisitas").text("0");
 	});
 }
